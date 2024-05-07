614f3eacc2c63a2b48c7236d0cba7a938976e9f6
sy-hiro <oneok.yukki127@gmail.com>
2024-05-07 23:20:53 +0900

    fix code re-review

--- kadai-java-review01/src/Review01.java
+++ kadai-java-review01/src/Review01.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
 public class Review01 {
-	
+    
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ//		
-		int price = 100;
-		int tax = 1;
-		int result = taxincluded(price,tax);
+		int price = 0;
+		double taxRate = 0.1;
 		
 		Scanner stdIn = new Scanner(System.in);
-		System.out.println("商品価格：");
+		System.out.print(" 販売価格を入力してください。:");
 		price = stdIn.nextInt();
-		System.out.println("消費税額：");
-		tax = stdIn.nextInt();
 		
-		result = (int) (price + tax);
-		System.out.println(price + "円の商品の税込価格は" + result + "円（消費税は"+ tax + "円）です。");
+		int taxResult = tax(price,taxRate);
+		int totalResult = price + taxResult;
+		
+		System.out.println(price + "円の商品の税込価格は" + totalResult + "円（消費税は"+ taxResult + "円）です。");
 		
 		stdIn.close();
 	}
 
-	public static int taxincluded(int price,int tax) {
-		int result = (int) (price + tax);
-        return result;
+	public static int tax(int price , double taxRate) {
+		System.out.println("debug: "+(int)(price * taxRate));
+		return (int)(price * taxRate) ;
 	}
-
 }
